2314bad65028790eca05203a09f01791602b8389
Jenia Berestian <jberestian@gmail.com>
2022-08-22 10:27:07 +0300

    added 1-2 task in hw

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,32 @@
+import data from 'data/data';
+
+import './statistics.css';
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      <h2 className="title">Upload stats</h2>
+
+      <ul className="stat-list">
+        <li className="item">
+          <span className="label">{data.label}</span>
+          <span className="percentage">{data.percentage}4</span>
+        </li>
+        <li className="item">
+          <span className="label">.mp3</span>
+          <span className="percentage">14%</span>
+        </li>
+        <li className="item">
+          <span className="label">.pdf</span>
+          <span className="percentage">41%</span>
+        </li>
+        <li className="item">
+          <span className="label">.mp4</span>
+          <span className="percentage">12%</span>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Statistics;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import Profile from 'components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
+import data from './data/data';
+
 import './index.css';
+import './reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile />
+    <Statistics title="Upload stats" stats={data} />
   </React.StrictMode>
 );
